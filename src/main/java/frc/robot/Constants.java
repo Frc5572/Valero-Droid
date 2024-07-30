d6a1ba6f1369d0635d04f503b138fd7397530f12
Alex Resnick <adr8292@gmail.com>
2024-07-29 19:11:36 -0500

    Fix Drive (#8)

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -104,7 +104,7 @@ public final class Constants {
         public static final double closedLoopRamp = 0.0;
 
         /* Angle Motor PID Values */
-        public static final double angleKP = 1.0;
+        public static final double angleKP = 5.0;
         public static final double angleKI = 0.0;
         public static final double angleKD = 0.0;
 
